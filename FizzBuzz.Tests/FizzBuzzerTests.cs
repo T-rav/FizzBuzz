9fd4fb45cd82a10652d5b5b9e031e6f4389d4e67
T-rav <tmfrisinger@gmail.com>
2017-07-28 13:58:44 +0200

    Setup project

---
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Method_Whenx_Shouldy()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            Assert.Fail("Test Not Yet Implemented");
+        }
+    }
+}
