5c2d51b925ed51b3be660548692dd93a3e9c5b6f
T-rav <tmfrisinger@gmail.com>
2017-09-01 13:43:23 +0200

    Refactored Test into TestCases

--- FizzBuzz.Tests/FizzBuzzerTests.cs
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -5,38 +5,12 @@ namespace FizzBuzz.Tests
     [TestFixture]
     public class FizzBuzzerTests
     {
-        [Test]
-        public void GetResult_WhenInputOne_ShouldReturnOne()
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(4, "4")]
+        public void GetResult_WhenNumberNotDivisibleBy3or5_ShouldReturnNumber(int input, string expected)
         {
             //---------------Set up test pack-------------------
-            var input = 1;
-            var expected = "1";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputTwo_ShouldReturnTwo()
-        {
-            //---------------Set up test pack-------------------
-            var input = 2;
-            var expected = "2";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputFour_ShouldReturnFour()
-        {
-            //---------------Set up test pack-------------------
-            var input = 4;
-            var expected = "4";
             var fizzBuzzer = new FizzBuzzer();
             //---------------Execute Test ----------------------
             var result = fizzBuzzer.GetResult(input);
@@ -44,12 +18,12 @@ namespace FizzBuzz.Tests
             Assert.AreEqual(expected, result);
         }
  
-        [Test]
-        public void GetResult_WhenInputThree_ShouldReturnFizz()
+        [TestCase(3, "Fizz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        public void GetResult_WhenNumberDivisibleBy3_ShouldReturnFizz(int input, string expected)
         {
             //---------------Set up test pack-------------------
-            var input = 3;
-            var expected = "Fizz";
             var fizzBuzzer = new FizzBuzzer();
             //---------------Execute Test ----------------------
             var result = fizzBuzzer.GetResult(input);
@@ -57,12 +31,12 @@ namespace FizzBuzz.Tests
             Assert.AreEqual(expected, result);
         }
 
-        [Test]
-        public void GetResult_WhenInputSix_ShouldReturnFizz()
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(20, "Buzz")]
+        public void GetResult_WhenNumberDivisibleBy5_ShouldReturnBuzz(int input, string expected)
         {
             //---------------Set up test pack-------------------
-            var input = 6;
-            var expected = "Fizz";
             var fizzBuzzer = new FizzBuzzer();
             //---------------Execute Test ----------------------
             var result = fizzBuzzer.GetResult(input);
@@ -70,90 +44,12 @@ namespace FizzBuzz.Tests
             Assert.AreEqual(expected, result);
         }
 
-        [Test]
-        public void GetResult_WhenInputNine_ShouldReturnFizz()
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(75, "FizzBuzz")]
+        public void GetResult_WhenNumberDivisibleBy3and5_ShouldReturnFizzBuzz(int input, string expected)
         {
             //---------------Set up test pack-------------------
-            var input = 9;
-            var expected = "Fizz";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputFive_ShouldReturnBuzz()
-        {
-            //---------------Set up test pack-------------------
-            var input = 5;
-            var expected = "Buzz";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputTen_ShouldReturnBuzz()
-        {
-            //---------------Set up test pack-------------------
-            var input = 10;
-            var expected = "Buzz";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputTwenty_ShouldReturnBuzz()
-        {
-            //---------------Set up test pack-------------------
-            var input = 20;
-            var expected = "Buzz";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputFifteen_ShouldReturnFizzBuzz()
-        {
-            //---------------Set up test pack-------------------
-            var input = 15;
-            var expected = "FizzBuzz";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputThirty_ShouldReturnFizzBuzz()
-        {
-            //---------------Set up test pack-------------------
-            var input = 30;
-            var expected = "FizzBuzz";
-            var fizzBuzzer = new FizzBuzzer();
-            //---------------Execute Test ----------------------
-            var result = fizzBuzzer.GetResult(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void GetResult_WhenInputSeventyFive_ShouldReturnFizzBuzz()
-        {
-            //---------------Set up test pack-------------------
-            var input = 75;
-            var expected = "FizzBuzz";
             var fizzBuzzer = new FizzBuzzer();
             //---------------Execute Test ----------------------
             var result = fizzBuzzer.GetResult(input);
